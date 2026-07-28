68e0e5382bf37005d3d8aea22464e6f9d916702b
crypt-farouk <faroukyusuf97@gmail.com>
2026-07-28 11:03:00 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("hello class");
+// alert("welcome to my website") ;
+
+let traineeName = "zuzu";
+let score = 58;
+let isPresent = true;
+
+
+console.log(traineeName);
+
+
+
+greetUser();
+function greetuser() {
+    alert("Hello, user!");
+}
+
+
+let messageBtn = document.getElementById("messageBtn");
+
+messageBtn.addEventListener("click", function () {
+    alert("Button Clicked");
+});
